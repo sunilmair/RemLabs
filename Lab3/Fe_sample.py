404e517ee7338f1573a4c6a60716fa3530c9e4a4
Sunil Mair <sunil.r.mair@gmail.com>
2020-10-20 04:33:50 -0400

    'test'

--- Lab3/Fe_sample.py
+++ Lab3/Fe_sample.py
@@ -22,6 +22,7 @@ def make_struc(alat):
     print(structure.species)
     return structure
 
+
 def make_bcc_struc(alat):
     """
     Creates the crystal structure using ASE.
@@ -33,6 +34,22 @@ def make_bcc_struc(alat):
     #write('s.cif', gecell)
     print(fecell, fecell.get_atomic_numbers())
     #fecell.set_atomic_numbers([26, 27])
+    structure = Struc(ase2struc(fecell))
+    print(structure.species)
+    return structure
+
+
+def make_hcp_struc(alat, clat):
+    """
+    Creates the crystal structure using ASE.
+    :param alat: Lattice parameter in angstrom
+    :return: structure object converted from ase
+    """
+    fecell = bulk('Fe', 'hcp', a=alat, c=clat)
+    # check how your cell looks like
+    #write('s.cif', gecell)
+    print(fecell, fecell.get_atomic_numbers())
+    fecell.set_atomic_numbers([26, 27])
     structure = Struc(ase2struc(fecell))
     print(structure.species)
     return structure
@@ -53,6 +70,102 @@ def compute_energy(alat, nk, ecut):
     kpts = Kpoints(gridsize=[nk, nk, nk], option='automatic', offset=False)
     dirname = 'Fe_a_{}_ecut_{}_nk_{}'.format(alat, ecut, nk)
     runpath = Dir(path=os.path.join(os.environ['WORKDIR'], "Lab3/Problem1", dirname))
+    input_params = PWscf_inparam({
+        'CONTROL': {
+            'calculation': 'scf',
+            'pseudo_dir': os.environ['ESPRESSO_PSEUDO'],
+            'outdir': runpath.path,
+            'tstress': True,
+            'tprnfor': True,
+            'disk_io': 'none',
+        },
+        'SYSTEM': {
+            'ecutwfc': ecut,
+            'ecutrho': ecut * 8,
+            'nspin': 2,
+            'starting_magnetization(1)': 0.7,
+            'occupations': 'smearing',
+            'smearing': 'mp',
+            'degauss': 0.02
+             },
+        'ELECTRONS': {
+            'diagonalization': 'david',
+            'mixing_beta': 0.5,
+            'conv_thr': 1e-7,
+        },
+        'IONS': {},
+        'CELL': {},
+        })
+
+    output_file = run_qe_pwscf(runpath=runpath, struc=struc,  pseudopots=pseudopots,
+                               params=input_params, kpoints=kpts, ncpu=2)
+    output = parse_qe_pwscf_output(outfile=output_file)
+    return output
+
+
+def compute_hcp_energy(alat, clat, nk, ecut):
+    """
+    Make an input template and select potential and structure, and the path where to run
+    """
+    potname = 'Fe.pbe-nd-rrkjus.UPF'
+    potpath = os.path.join(os.environ['ESPRESSO_PSEUDO'], potname)
+    pseudopots = {'Fe': PseudoPotential(path=potpath, ptype='uspp', element='Fe',
+                                        functional='GGA', name=potname),
+                  'Co': PseudoPotential(path=potpath, ptype='uspp', element='Fe',
+                                        functional='GGA', name=potname)
+                  }
+    struc = make_hcp_struc(alat=alat, clat=clat)
+    kpts = Kpoints(gridsize=[2*nk, 2*nk, nk], option='automatic', offset=False)
+    dirname = 'Fe_hcp_a_{}_ecut_{}_nk_{}'.format(alat, ecut, nk)
+    runpath = Dir(path=os.path.join(os.environ['WORKDIR'], "Lab3/Problem1B/hcp", dirname))
+    input_params = PWscf_inparam({
+        'CONTROL': {
+            'calculation': 'scf',
+            'pseudo_dir': os.environ['ESPRESSO_PSEUDO'],
+            'outdir': runpath.path,
+            'tstress': True,
+            'tprnfor': True,
+            'disk_io': 'none',
+        },
+        'SYSTEM': {
+            'ecutwfc': ecut,
+            'ecutrho': ecut * 8,
+            'nspin': 2,
+            'starting_magnetization(1)': 0.7,
+            'occupations': 'smearing',
+            'smearing': 'mp',
+            'degauss': 0.02
+             },
+        'ELECTRONS': {
+            'diagonalization': 'david',
+            'mixing_beta': 0.5,
+            'conv_thr': 1e-7,
+        },
+        'IONS': {},
+        'CELL': {},
+        })
+
+    output_file = run_qe_pwscf(runpath=runpath, struc=struc,  pseudopots=pseudopots,
+                               params=input_params, kpoints=kpts, ncpu=2)
+    output = parse_qe_pwscf_output(outfile=output_file)
+    return output
+
+
+def compute_bcc_energy(alat, nk, ecut):
+    """
+    Make an input template and select potential and structure, and the path where to run
+    """
+    potname = 'Fe.pbe-nd-rrkjus.UPF'
+    potpath = os.path.join(os.environ['ESPRESSO_PSEUDO'], potname)
+    pseudopots = {'Fe': PseudoPotential(path=potpath, ptype='uspp', element='Fe',
+                                        functional='GGA', name=potname),
+                  'Co': PseudoPotential(path=potpath, ptype='uspp', element='Fe',
+                                        functional='GGA', name=potname)
+                  }
+    struc = make_bcc_struc(alat=alat, clat=clat)
+    kpts = Kpoints(gridsize=[nk, nk, nk], option='automatic', offset=False)
+    dirname = 'Fe_bcc_a_{}_ecut_{}_nk_{}'.format(alat, ecut, nk)
+    runpath = Dir(path=os.path.join(os.environ['WORKDIR'], "Lab3/Problem1B/bcc", dirname))
     input_params = PWscf_inparam({
         'CONTROL': {
             'calculation': 'scf',
@@ -242,7 +355,36 @@ def relax_scan_bcc_nk_conv():
     print(output)
 
 
+def volume_scan_hcp():
+    nk = 7
+    ecut = 30
+
+    c_over_a = 4.01870773/2.469607124
+    equil_a = 2.469607124
+
+    max_a_multiplier = 1.05
+    min_a_multiplier = 0.9
+
+    alat_list = numpy.linspace(min_a_multiplier*equil_a, max_a_multiplier*equil_a, 5)
+
+    output = [compute_hcp_energy(alat, c_over_a*alat, nk, ecut) for alat in alat_list]
+    print(output)
+
+
+def volume_scan_bcc():
+    nk = 14
+    ecut = 30
+
+    equil_a = 2.845479126
+
+    max_a_multiplier = 1.05
+    min_a_multiplier = 0.9
+
+    alat_list = numpy.linspace(min_a_multiplier*equil_a, max_a_multiplier*equil_a, 5)
+
+    output = [compute_bcc_energy(alat, nk, ecut) for alat in alat_list]
+    print(output)
+
 if __name__ == '__main__':
     # put here the function that you actually want to run
-    #lattice_scan()
-    relax_scan_bcc_nk_conv()
+    volume_scan_hcp()
