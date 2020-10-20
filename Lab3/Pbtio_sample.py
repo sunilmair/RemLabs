3a77e2553f1fc0c492dd857529ebd121ed1200be
Sunil Mair <sunil.r.mair@gmail.com>
2020-10-20 19:15:38 -0400

    'test'

--- Lab3/Pbtio_sample.py
+++ Lab3/Pbtio_sample.py
@@ -34,7 +34,7 @@ def compute_energy(alat, nk, ecut, displ=0):
     constraint = Constraint(atoms={'0': [0,0,0], '1': [0,0,0]})
     kpts = Kpoints(gridsize=[nk, nk, nk], option='automatic', offset=True)
     dirname = 'PbTiO3_a_{}_ecut_{}_nk_{}_displ_{}'.format(alat, ecut, nk, displ)
-    runpath = Dir(path=os.path.join(os.environ['WORKDIR'], "Lab3/Problem2", dirname))
+    runpath = Dir(path=os.path.join(os.environ['WORKDIR'], "Lab3/Problem2/a/conv", dirname))
     input_params = PWscf_inparam({
         'CONTROL': {
             'calculation': 'scf',
@@ -65,9 +65,94 @@ def compute_energy(alat, nk, ecut, displ=0):
     return output
 
 
+def compute_Ti_disp_energy(alat, nk, ecut, displ=0):
+    """
+    Make an input template and select potential and structure, and the path where to run
+    """
+    pseudopots = {'Pb': PseudoPotential(ptype='uspp', element='Pb', functional='LDA', name='Pb.pz-d-van.UPF'),
+                  'Ti': PseudoPotential(ptype='uspp', element='Ti', functional='LDA', name='Ti.pz-sp-van_ak.UPF'),
+                  'O': PseudoPotential(ptype='uspp', element='O', functional='LDA', name='O.pz-rrkjus.UPF')}
+    struc = make_struc(alat=alat, displacement=displ)
+    # fix the Pb and Ti atoms in place during relaxation
+    constraint = Constraint(atoms={'0': [0,0,0], '1': [0,0,0]})
+    kpts = Kpoints(gridsize=[nk, nk, nk], option='automatic', offset=True)
+    dirname = 'PbTiO3_a_{}_ecut_{}_nk_{}_displ_{}'.format(alat, ecut, nk, displ)
+    runpath = Dir(path=os.path.join(os.environ['WORKDIR'], "Lab3/Problem2/b", dirname))
+    input_params = PWscf_inparam({
+        'CONTROL': {
+            'calculation': 'relax',
+            'pseudo_dir': os.environ['ESPRESSO_PSEUDO'],
+            'outdir': runpath.path,
+            'tstress': True,
+            'tprnfor': True,
+            'disk_io': 'none'
+        },
+        'SYSTEM': {
+            'ecutwfc': ecut,
+            'ecutrho': ecut * 8,
+             },
+        'ELECTRONS': {
+            'diagonalization': 'david',
+            'mixing_beta': 0.7,
+            'conv_thr': 1e-7,
+        },
+        'IONS': {
+            'ion_dynamics': 'bfgs'
+        },
+        'CELL': {},
+        })
+
+    output_file = run_qe_pwscf(runpath=runpath, struc=struc,  pseudopots=pseudopots,
+                               params=input_params, kpoints=kpts, constraint=constraint, ncpu=2)
+    output = parse_qe_pwscf_output(outfile=output_file)
+    return output
+
+
+def compute_Ti_relax(alat, nk, ecut, displ=0):
+    """
+    Make an input template and select potential and structure, and the path where to run
+    """
+    pseudopots = {'Pb': PseudoPotential(ptype='uspp', element='Pb', functional='LDA', name='Pb.pz-d-van.UPF'),
+                  'Ti': PseudoPotential(ptype='uspp', element='Ti', functional='LDA', name='Ti.pz-sp-van_ak.UPF'),
+                  'O': PseudoPotential(ptype='uspp', element='O', functional='LDA', name='O.pz-rrkjus.UPF')}
+    struc = make_struc(alat=alat, displacement=displ)
+    # fix the Pb and Ti atoms in place during relaxation
+    constraint = Constraint(atoms={'0': [0,0,0]})
+    kpts = Kpoints(gridsize=[nk, nk, nk], option='automatic', offset=True)
+    dirname = 'PbTiO3_a_{}_ecut_{}_nk_{}_displ_{}'.format(alat, ecut, nk, displ)
+    runpath = Dir(path=os.path.join(os.environ['WORKDIR'], "Lab3/Problem2/c", dirname))
+    input_params = PWscf_inparam({
+        'CONTROL': {
+            'calculation': 'relax',
+            'pseudo_dir': os.environ['ESPRESSO_PSEUDO'],
+            'outdir': runpath.path,
+            'tstress': True,
+            'tprnfor': True,
+            'disk_io': 'none'
+        },
+        'SYSTEM': {
+            'ecutwfc': ecut,
+            'ecutrho': ecut * 8,
+             },
+        'ELECTRONS': {
+            'diagonalization': 'david',
+            'mixing_beta': 0.7,
+            'conv_thr': 1e-7,
+        },
+        'IONS': {
+            'ion_dynamics': 'bfgs'
+        },
+        'CELL': {},
+        })
+
+    output_file = run_qe_pwscf(runpath=runpath, struc=struc,  pseudopots=pseudopots,
+                               params=input_params, kpoints=kpts, constraint=constraint, ncpu=2)
+    output = parse_qe_pwscf_output(outfile=output_file)
+    return output
+
 
 def lattice_scan():
-    nk = 3
+    nk = 4
     ecut = 30
     alat_list = numpy.linspace(3.8, 4.0, 11)
     print(alat_list)
@@ -82,6 +167,52 @@ def lattice_scan():
     plt.show()
 
 
+def problem2_ecut_conv():
+    nk = 4
+    ecut_list = numpy.arange(10.0, 40.0, 5)
+    alat = 3.97
+    output = [compute_energy(alat=alat, ecut=ecut, nk=nk)['energy'] for ecut in ecut_list]
+
+    print(ecut_list)
+    print(output)
+
+
+def problem2a_lattice_scan():
+    #change out dir
+    nk = 4
+    ecut = 30 #change based on conv
+    alat_list = numpy.linspace(3.9, 4.1, 11)
+
+    output = [compute_energy(alat=alat, ecut=ecut, nk=nk)['energy'] for alat in alat_list]
+
+    print(alat_list)
+    print(output)
+
+
+def problem2b_Ti_disp_scan():
+    nk = 4
+    ecut = 30 #change based on conv
+    alat = 3.97 #change based on eq
+    disp_list = numpy.linspace(0, 0.05, 11)
+
+    output = [compute_Ti_disp_energy(alat=alat, ecut=ecut, nk=nk, displ=disp)['energy'] for disp in disp_list]
+
+    print(disp_list)
+    print(output)
+
+
+def problem2c_Ti_relax():
+    nk = 4
+    ecut = 30  # change based on conv
+    alat = 3.97  # change based on eq
+    disp = 0.01 # change based on eq
+
+    output = compute_Ti_relax(alat=alat, ecut=ecut, nk=nk, displ=disp)
+    print(output)
+
 if __name__ == '__main__':
     # put here the function that you actually want to run
-    lattice_scan()
+    problem2_ecut_conv()
+    #problem2a_lattice_scan()
+    #problem2b_Ti_disp_scan()
+    #problem2c_Ti_relax()
