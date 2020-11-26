9ebbaa4de3acb20ea677a0570500b7422b4246fd
Sunil Mair <sunil.r.mair@gmail.com>
2020-11-26 04:01:27 -0500

    'test'

--- Project/LiSi_run.py
+++ Project/LiSi_run.py
@@ -2,14 +2,18 @@ from LiSi_structures import *
 from LiSi_LAMMPS_templates import *
 import time
 
+project_full_path = '/home/modeler/RemLabs/Project'
 def Si_lattice_constant_calculation():
     """
     Runs an optimization calculation on an Si unitcell to find the equilibrium lattice parameter using the 2NN MEAM
     """
     timestamp = time.strftime('%Y%m%d-%H%M%S')
-    runpath = Dir(path=os.path.join('/home/modeler/RemLabs/Project/Si_lattice_constant', timestamp))
+    path = os.path.join(project_full_path, 'Si_lattice_constant', timestamp)
+
+    runpath = Dir(path=path)
     struc = make_Si_unitcell()
-    inparam = {'OUTFILE': os.path.join('/home/modeler/RemLabs/Project/Si_lattice_constant', timestamp)}
+    inparam = {'OUTFILE': path}
+
     output_file = lammps_run(struc=struc, runpath=runpath, potential=False, intemplate=relaxation_calculation_template, inparam=inparam)
     energy, lattice = get_lammps_energy(outfile=output_file)
     return energy, lattice
@@ -20,9 +24,12 @@ def Si_unitcell_central_Li_relaxation():
     Runs an optimization calculation on an Si unitcell with an Li in the central tetrahedral site
     """
     timestamp = time.strftime('%Y%m%d-%H%M%S')
-    runpath = Dir(path=os.path.join('/home/modeler/RemLabs/Project/Si_unitcell_central_Li_relaxation', timestamp))
+    path = os.path.join(project_full_path, 'Si_unitcell_central_Li_relaxation', timestamp)
+
+    runpath = Dir(path=path)
     struc = make_unitcell_central_Li()
-    inparam = {'OUTFILE': os.path.join('/home/modeler/RemLabs/Project/Si_unitcell_central_Li_relaxation', timestamp)}
+    inparam = {'OUTFILE': path}
+
     output_file = lammps_run(struc=struc, runpath=runpath, potential=False, intemplate=relaxation_calculation_template, inparam=inparam)
     energy, lattice = get_lammps_energy(outfile=output_file)
     return energy, lattice
