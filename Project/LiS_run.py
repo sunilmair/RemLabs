de217f865aba55696a752ab984692cf39f7adb35
Sunil Mair <sunil.r.mair@gmail.com>
2020-11-26 06:54:23 +0000

    test

--- Project/LiS_run.py
+++ Project/LiS_run.py
@@ -6,9 +6,9 @@ def Si_lattice_constant_calculation():
     """
     Runs a optimization calculation on an Si unitcell to find the equilibrium lattice parameter using the 2NN MEAM
     """
-    runpath = Dir(path=os.path.join('Si_lattice_constant', time.strftime(%Y%m%d-%H%M%S)))
+    runpath = Dir(path=os.path.join('Si_lattice_constant', time.strftime('%Y%m%d-%H%M%S')))
     struc = make_Si_unitcell()
-    output_file = lammps_run(struc=struc, runpath=runpath, intemplate=Si_lattice_constant_template, inparam={})
+    output_file = lammps_run(struc=struc, runpath=runpath, potential=False, intemplate=Si_lattice_constant_template, inparam={})
     energy, lattice = get_lammps_energy(outfile=output_file)
     return energy, lattice
 
