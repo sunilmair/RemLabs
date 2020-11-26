73b9769ee19a610505f6c901e4d52a434f8a07d7
Sunil Mair <sunil.r.mair@gmail.com>
2020-11-26 04:54:20 -0500

    'test'

--- Project/LiSi_structures.py
+++ Project/LiSi_structures.py
@@ -32,7 +32,7 @@ def make_Li_filled_unitcell(write_file=False):
     unitcell.extend(Atoms('Li'+str(len(Li_fractional_sites)),
                           positions=[tuple([frac_coord*Si_alat for frac_coord in Li_frac_site])
                                      for Li_frac_site in Li_fractional_sites]))
-    if write_file : write('Li_filled_unitcell.cif', unitcell)
+    if write_file : write(structures_folder_path + 'Li_filled_unitcell.cif', unitcell)
     return Struc(ase2struc(unitcell))
 
 
@@ -42,7 +42,7 @@ def make_unitcell_central_Li(write_file=False):
     """
     unitcell = crystal('Si', [(0, 0, 0)], spacegroup=227, cellpar=3 * [Si_alat] + 3 * [90])
     unitcell.extend(Atoms('Li', positions=[tuple(3 * [0.5 * Si_alat])]))
-    if write_file : write('unitcell_central_Li.cif', unitcell)
+    if write_file : write(structures_folder_path + 'unitcell_central_Li.cif', unitcell)
     return Struc(ase2struc(unitcell))
 
 
@@ -52,7 +52,7 @@ def make_unitcell_neighbor_central_Li(write_file=False):
     """
     unitcell = crystal('Si', [(0, 0, 0)], spacegroup=227, cellpar=3 * [Si_alat] + 3 * [90])
     unitcell.extend(Atoms('Li', positions=[tuple(3 * [0.75 * Si_alat])]))
-    if write_file : write('unitcell_neighbor_central_Li.cif', unitcell)
+    if write_file : write(structures_folder_path + 'unitcell_neighbor_central_Li.cif', unitcell)
     return Struc(ase2struc(unitcell))
 
 
@@ -63,7 +63,7 @@ def make_2x2x2_supercell_1x1x1_central_Li(write_file=False):
     unitcell = crystal('Si', [(0, 0, 0)], spacegroup=227, cellpar=3 * [Si_alat] + 3 * [90])
     supercell = make_supercell(unitcell, np.identity(3) * 2)
     supercell.extend(Atoms('Li', positions=[tuple(3 * [0.5 * Si_alat])]))
-    if write_file : write('2x2x2_supercell_1x1x1_central_Li.cif', supercell)
+    if write_file : write(structures_folder_path + '2x2x2_supercell_1x1x1_central_Li.cif', supercell)
     return Struc(ase2struc(supercell))
 
 
@@ -74,7 +74,7 @@ def make_2x2x2_supercell_1x1x1_neighbor_central_Li(write_file=False):
     unitcell = crystal('Si', [(0, 0, 0)], spacegroup=227, cellpar=3 * [Si_alat] + 3 * [90])
     supercell = make_supercell(unitcell, np.identity(3) * 2)
     supercell.extend(Atoms('Li', positions=[tuple(3 * [0.75 * Si_alat])]))
-    if write_file : write('2x2x2_supercell_1x1x1_neighbor_central_Li.cif', supercell)
+    if write_file : write(structures_folder_path + '2x2x2_supercell_1x1x1_neighbor_central_Li.cif', supercell)
     return Struc(ase2struc(supercell))
 
 
@@ -85,7 +85,7 @@ def make_3x3x3_supercell_central_Li(write_file=False):
     unitcell = crystal('Si', [(0, 0, 0)], spacegroup=227, cellpar=3 * [Si_alat] + 3 * [90])
     supercell = make_supercell(unitcell, np.identity(3)*3)
     supercell.extend(Atoms('Li', positions=[tuple(3*[1.5*Si_alat])]))
-    if write_file : write('3x3x3_supercell_central_Li.cif', supercell)
+    if write_file : write(structures_folder_path + '3x3x3_supercell_central_Li.cif', supercell)
     return Struc(ase2struc(supercell))
 
 
@@ -96,16 +96,16 @@ def make_3x3x3_supercell_neighbor_central_Li(write_file=False):
     unitcell = crystal('Si', [(0, 0, 0)], spacegroup=227, cellpar=3 * [Si_alat] + 3 * [90])
     supercell = make_supercell(unitcell, np.identity(3) * 3)
     supercell.extend(Atoms('Li', positions=[tuple(3 * [1.75 * Si_alat])]))
-    if write_file : write('3x3x3_supercell_neighbor_central_Li.cif', supercell)
+    if write_file : write(structures_folder_path + '3x3x3_supercell_neighbor_central_Li.cif', supercell)
     return Struc(ase2struc(supercell))
 
 
 if __name__ == "__main__":
     make_Si_unitcell(True)
-    #make_Li_filled_unitcell(True)
-    #make_unitcell_central_Li(True)
-    #make_unitcell_neighbor_central_Li(True)
-    #make_2x2x2_supercell_1x1x1_central_Li(True)
-    #make_2x2x2_supercell_1x1x1_neighbor_central_Li(True)
-    #make_3x3x3_supercell_central_Li(True)
-    #make_3x3x3_supercell_neighbor_central_Li(True)
+    make_Li_filled_unitcell(True)
+    make_unitcell_central_Li(True)
+    make_unitcell_neighbor_central_Li(True)
+    make_2x2x2_supercell_1x1x1_central_Li(True)
+    make_2x2x2_supercell_1x1x1_neighbor_central_Li(True)
+    make_3x3x3_supercell_central_Li(True)
+    make_3x3x3_supercell_neighbor_central_Li(True)
