62c620a9820302a508425673522ab6216268eea9
Sunil Mair <sunil.r.mair@gmail.com>
2020-12-03 19:26:42 -0500

    'test'

--- Project/LiSi_run.py
+++ Project/LiSi_run.py
@@ -34,6 +34,23 @@ def Si_unitcell_central_Li_relaxation():
     energy, lattice = get_lammps_energy(outfile=output_file)
     return energy, lattice
 
+
+def Si_3x3x3_supercell_central_Li_relaxation():
+    """
+    Runs an optimization calculation on an Si unitcell with an Li in the central tetrahedral site
+    """
+    timestamp = time.strftime('%Y%m%d-%H%M%S')
+    path = os.path.join(project_full_path, 'Si_3x3x3_supercell_central_Li_relaxation', timestamp)
+
+    runpath = Dir(path=path)
+    struc = make_3x3x3_supercell_central_Li()
+    inparam = {'OUTFILE': path}
+
+    output_file = lammps_run(struc=struc, runpath=runpath, potential=False, intemplate=relaxation_calculation_template, inparam=inparam)
+    energy, lattice = get_lammps_energy(outfile=output_file)
+    return energy, lattice
+
 if __name__ == "__main__":
-    Si_lattice_constant_calculation()
+    #Si_lattice_constant_calculation()
     #Si_unitcell_central_Li_relaxation()
+    Si_3x3x3_supercell_central_Li_relaxation()
