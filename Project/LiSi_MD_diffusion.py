2b937c12de1caabf5330d4fc3bc94eb93576a4b2
Sunil Mair <sunil.r.mair@gmail.com>
2020-12-04 02:30:49 -0500

    'test'

--- Project/LiSi_MD_diffusion.py
+++ Project/LiSi_MD_diffusion.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 project_full_path = '/home/modeler/RemLabs/Project'
 
-def Si_n3_supercell_run_MD(n, temperature, timestep, nsteps, filepath):
+def Si_n3_supercell_run_MD(n, T, timestep, nsteps, filepath):
     """
     Runs an npt ensemble to track the MSD of a Li in a 3x3x3 Si supercell to calculate the diffusion coefficient
     """
@@ -17,7 +17,7 @@ def Si_n3_supercell_run_MD(n, temperature, timestep, nsteps, filepath):
     struc = make_n3_supercell_1x1x1_central_Li(n)
     inparam = {
         'OUTFILE': path,
-        'TEMPERATURE': temperature,
+        'TEMPERATURE': T,
         'NSTEPS': nsteps,
         'TIMESTEP': timestep,
         'TOUTPUT': 100,  # how often to write thermo output
@@ -72,21 +72,5 @@ def evaluate_timestep():
     fig.savefig(filepath+'/timestep_convergence-' + time.strftime('%Y%m%d-%H%M%S'))
 
 
-def Si_3x3x3_supercell_Li_MSD_vs_time(Tstart, Tstop, numT):
-    timestep = 0.001
-    total_time = 10
-    equilibration_time =
-    fig, (ax_left, ax_right) = plt.subplots(1, 2, figsize=(18, 6))
-    for T in np.linspace(Tstart, Tstop, numT):
-        output = Si_3x3x3_supercell_run_MD(T, 0.005, 5000)
-        ax_left.plot(output[0], output[4])
-        ax_right.plot(output[0], output[-1], label=str(T))
-    ax_left.set_ylabel('Temperature')
-    ax_right.set_ylabel('Li MSD')
-    plt.legend()
-    fig.savefig('Si_3x3x3_supercell_MD/Temp_and_MSD-' + time.strftime('%Y%m%d-%H%M%S'))
-
-
 if __name__ == "__main__":
-    #Si_3x3x3_supercell_Li_MSD_vs_time(600, 1800, 7)
     evaluate_timestep()
